c104230f8dfb96a6014285c65d56c373854d0c3f
Gabidodson <dodsongabi@gmail.com>
2024-12-05 16:26:04 -0500

    Fetch chart data from JSON file

--- react-chart-app/src/App.jsx
+++ react-chart-app/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Fetch Data for Charts
+import React, { useEffect, useState } from 'react'
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+import ScatterChart from './components/ScatterChart';
+import BubbleChart from './components/BubbleChart';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chartData, setChartData] = useState(null);
 
-  return (
-    <>
+  useEffect(()=>{
+    fetch ('/financial_data.json')
+    .then (response => response.json())
+    .then(data => setChartData(data));
+  },[]);
+  if (!chartData) {
+  return <div>Loading...</div>;
+  }
+  return(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <h1>Financial Dashboared</h1>
+        <div style={{padding:'20px'}}>
+          <BarChart data={chartData}/>
+          <LineChart data={chartData}/>
+          <ScatterChart data={chartData}/>
+          <BubbleChart data={chartData}/>
+        </div>
+        </div>
+  );
 }
 
-export default App
+export default App;
